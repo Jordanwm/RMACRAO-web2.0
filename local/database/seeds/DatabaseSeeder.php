ddb121a3767ccf4276a07ae883bf2d84b255cfcf
Jordanwm <jmoser@mymail.mines.edu>
2015-06-10 22:49:14 -0600

    Added RMACRAO emails and set public path

--- local/database/seeds/DatabaseSeeder.php
+++ local/database/seeds/DatabaseSeeder.php
@@ -17,6 +17,24 @@ class DatabaseSeeder extends Seeder {
 
 		User::create(['email'=>'sasquatch1994@gmail.com']);
 		User::create(['email'=>'jmoser@mymail.mines.edu']);
+		User::create(['email'=>'Tmcclure@mymail.mines.edu']);
+		User::create(['email'=>'Mccluretaylor11@gmail.com']);
+
+		// RMACRAO ACCOUNTS
+		User::create(['email'=>'president@rmacrao.org']);
+		User::create(['email'=>'presidentelect@rmacrao.org']);
+		User::create(['email'=>'pastpresident@rmacrao.org']);
+		User::create(['email'=>'vpwyoming@rmacrao.org']);
+		User::create(['email'=>'vpcolorado@rmacrao.org']);
+		User::create(['email'=>'vpnewmexico@rmacrao.org']);
+		User::create(['email'=>'secretary@rmacrao.org']);
+		User::create(['email'=>'historian@rmacrao.org']);
+		User::create(['email'=>'exhibitors@rmacrao.org']);
+		User::create(['email'=>'treasurer@rmacrao.org']);
+		User::create(['email'=>'treasurerelect@rmacrao.org']);
+		User::create(['email'=>'communication@rmacrao.org']);
+		User::create(['email'=>'web@rmacrao.org']);
+		User::create(['email'=>'localarrangements@rmacrao.org']);
 		// $this->call('UserTableSeeder');
 	}
 
